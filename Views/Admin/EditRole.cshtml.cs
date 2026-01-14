32c8c8dfab58497f8bdd4f058da862bfc6b63d2e
alex-olar-2 <alex.olar2@student.usv.ro>
2026-01-14 22:28:03 +0200

    Add CRUD to Roles

---
+++ Views/Admin/EditRole.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExtractInfoIdentityDocument.Views.Admin
+{
+    public class EditRoleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
